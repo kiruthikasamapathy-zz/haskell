73e43036b6adc435107a34fcaaa9c2ee0601d98e
Kiruthika Samapathy <ksama@thoughtworks.com>
2015-05-24 21:59:33 +1000

    more shapes

--- customTypes/shapes.hs
+++ customTypes/shapes.hs
@@ -1,5 +1,6 @@
-data Square = Square Int
+data Shape = Square Int | Rect Int Int Int Int
 
-area :: Square -> Int
+area :: Shape -> Int
 
 area (Square side) = side ^ 2
+area (Rect x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
