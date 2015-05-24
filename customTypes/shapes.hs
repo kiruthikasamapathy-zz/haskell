331162bf9d9f0bf244622061b959b7086b272b46
Kiruthika Samapathy <ksama@thoughtworks.com>
2015-05-24 22:02:14 +1000

    data within custom data

--- customTypes/shapes.hs
+++ customTypes/shapes.hs
@@ -1,6 +1,7 @@
-data Shape = Square Int | Rect Int Int Int Int
+data Point = Point Int Int
+data Shape = Square Int | Rect Point Point
 
 area :: Shape -> Int
 
 area (Square side) = side ^ 2
-area (Rect x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+area (Rect (Point x1 y1)  (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
